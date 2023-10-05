fe7a244abf2660b6043f14a3be27876b91cd56fa
IvSDF <demchyshyn.ivan@gmail.com>
2023-10-05 23:08:21 +0300

    create_store_method_in_LinkController

--- routes/api.php
+++ routes/api.php
@@ -48,6 +48,7 @@ Route::prefix('ambassador')->group(function () {
     Route::get('products/backend', [ProductController::class, 'backend']);
 
     Route::middleware(['auth:sanctum', 'scope.ambassador'])->group(function (){
+        Route::post('links', [LinkController::class, 'store']);
         Route::get('stats', [StatsController::class, 'index']);
         Route::get('rankings', [StatsController::class, 'rankings']);
     });
