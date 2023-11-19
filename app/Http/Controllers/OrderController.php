a8089631ce76aded910d1e66d05c964faa59d2ff
IvSDF <demchyshyn.ivan@gmail.com>
2023-11-20 00:24:04 +0200

    add OrderController

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,13 +3,66 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\OrderResource;
+use App\Models\Link;
 use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
     public function index()
     {
         return OrderResource::collection(Order::with('orderItems')->get());
+    }
+
+    public function store(Request $request)
+    {
+        if (!$link = Link::where('code', $request->input('code'))->first()) {
+            abort(400, 'Invalid code');
+        }
+
+        try {
+
+            DB::beginTransaction();
+
+            $order = new Order();
+
+            $order->code = $link->code;
+            $order->user_id = $link->user->id;
+            $order->ambassador_email = $link->user->email;
+            $order->first_name = $request->input('first_name');
+            $order->last_name = $request->input('last_name');
+            $order->email = $request->input('email');
+            $order->address = $request->input('address');
+            $order->country = $request->input('country');
+            $order->city = $request->input('city');
+            $order->zip = $request->input('zip');
+
+            $order->save();
+
+            foreach ($request->input('products') as $item) {
+                $product = Product::find($item['product_id']);
+
+                $orderItem = new OrderItem();
+                $orderItem->order_id = $order->id;
+                $orderItem->product_title = $product->title;
+                $orderItem->price = $product->price;
+                $orderItem->quantity = $item['quantity'];
+                $orderItem->ambassador_revenue = 0.1 * $product->price * $item['quantity'];
+                $orderItem->admin_revenue = 0.9 * $product->price * $item['quantity'];
+
+                $orderItem->save();
+            }
+
+            DB::commit();
+        } catch ( \Throwable $e) {
+
+            DB::rollBack();
+            abort(500, 'Ups!!!');
+        }
+
+        return $order->load('orderItems');
     }
 }
